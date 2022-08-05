ddba06092c9beb0280b095f42f63e7b2558d9854
Sayantani96 <sayantani.h96@gmail.com>
2022-08-05 22:42:58 +0530

    Cart dropdown closed on clicking checkout button

--- src/components/routes/product/cart-dropdown.jsx
+++ src/components/routes/product/cart-dropdown.jsx
@@ -5,16 +5,20 @@ import './dropdown.scss'
 import { CartItemContext } from '../../../context/CartItemContext'
 import {useNavigate} from 'react-router-dom'
 const Dropdown = () => {
-  const {cartItems}=useContext(CartItemContext);
+  const {cartItems,setIsCartOpen}=useContext(CartItemContext);
   const navigate=useNavigate();
+  const cartCloseAndNavigate=()=>{
+    navigate('/checkout');
+    setIsCartOpen(false);
+  }
   return (
-    <div className='cart-dropdown-container'>
+    <div className='cart-dropdown-container'> 
       <div className='cart-items'>
        {
         cartItems.map((item)=><CartItem key={item.id} cartItem={item}/>)
        }
        </div>
-        <Button buttonText='Checkout' onClick={()=>navigate('/checkout')}/>
+        <Button buttonText='Checkout' onClick={cartCloseAndNavigate}/>
     </div>
   )
 }
